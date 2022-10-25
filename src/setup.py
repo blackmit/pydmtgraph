a31669640046d1ca863265ed45b3ac7489ddfe73
blackmit <blackmit@oregonstate.edu>
2022-10-25 16:42:11 -0700

    changed setup to work with different version of python

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,18 @@
+from platform import python_version_tuple
 from distutils.core import setup
 from distutils.extension import Extension
+
+# Boost has different library names
+# for different version of Python.
+py_version = python_version_tuple()
+libraries = [f"boost_python{py_version[0]}{py_version[1]}",
+             f"boost_numpy{py_version[0]}{py_version[1]}"]
 
 dmtgraph = Extension(
     "pydmtgraph.dmtgraph",
     sources=["pydmtgraph/dmtgraph/DMTGraph.cpp"],
     library_dirs=["${CONDA_PREFIX}/lib"], # uncomment if you downloaded boost with conda
-    libraries=["boost_python39", "boost_numpy39"],
+    libraries=libraries,
     extra_compile_args=["--std=c++11"],
 )
 
